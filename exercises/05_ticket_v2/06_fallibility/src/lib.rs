5a92fc97106b832836cab9cf5d08510285302418
sproj <jonesalan404@gmail.com>
2024-10-12 17:32:48 +0100

    05_06 fallibility with Result

--- exercises/05_ticket_v2/06_fallibility/src/lib.rs
+++ exercises/05_ticket_v2/06_fallibility/src/lib.rs
@@ -1,5 +1,7 @@
 // TODO: Convert the `Ticket::new` method to return a `Result` instead of panicking.
-//   Use `String` as the error type.
+//   Use `String` as the Error type.
+
+use std::io::Error;
 
 #[derive(Debug, PartialEq)]
 struct Ticket {
@@ -16,25 +18,25 @@ enum Status {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: Status) -> Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Result<Ticket, &'static str> {
         if title.is_empty() {
-            panic!("Title cannot be empty");
+            return Err("Title cannot be empty");
         }
         if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
+            return Err("Title cannot be longer than 50 bytes");
         }
         if description.is_empty() {
-            panic!("Description cannot be empty");
+            return Err("Description cannot be empty");
         }
         if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
+            return Err("Description cannot be longer than 500 bytes");
         }
 
-        Ticket {
+        Ok(Ticket {
             title,
             description,
             status,
-        }
+        })
     }
 }
 
@@ -45,27 +47,27 @@ mod tests {
 
     #[test]
     fn title_cannot_be_empty() {
-        let error = Ticket::new("".into(), valid_description(), Status::ToDo).unwrap_err();
-        assert_eq!(error, "Title cannot be empty");
+        let Error = Ticket::new("".into(), valid_description(), Status::ToDo).unwrap_err();
+        assert_eq!(Error, "Title cannot be empty");
     }
 
     #[test]
     fn description_cannot_be_empty() {
-        let error = Ticket::new(valid_title(), "".into(), Status::ToDo).unwrap_err();
-        assert_eq!(error, "Description cannot be empty");
+        let Error = Ticket::new(valid_title(), "".into(), Status::ToDo).unwrap_err();
+        assert_eq!(Error, "Description cannot be empty");
     }
 
     #[test]
     fn title_cannot_be_longer_than_fifty_chars() {
-        let error =
+        let Error =
             Ticket::new(overly_long_title(), valid_description(), Status::ToDo).unwrap_err();
-        assert_eq!(error, "Title cannot be longer than 50 bytes");
+        assert_eq!(Error, "Title cannot be longer than 50 bytes");
     }
 
     #[test]
     fn description_cannot_be_longer_than_500_chars() {
-        let error =
+        let Error =
             Ticket::new(valid_title(), overly_long_description(), Status::ToDo).unwrap_err();
-        assert_eq!(error, "Description cannot be longer than 500 bytes");
+        assert_eq!(Error, "Description cannot be longer than 500 bytes");
     }
 }
